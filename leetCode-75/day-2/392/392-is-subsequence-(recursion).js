beb05def9f469187a679a22c99f2c409efbac9e1
antonborodulin <aborodulin@planatechnologies.com>
2022-10-15 14:56:48 -0700

    +/- 392 Uppdate Solution + 392 Add alternative

--- leetCode-75/day-2/392/392-is-subsequence-(recursion).js
+++ leetCode-75/day-2/392/392-is-subsequence-(recursion).js
@@ -1,8 +1,16 @@
 /**
- * Runtime: 80  ms
+ * Runtime: 64  ms
  * Memory Usage: 42.6 MB
- * Link: https://leetcode.com/submissions/detail/757846937/
- /**
+ * Link: https://leetcode.com/problems/is-subsequence/submissions/823270282/
+ //-----------------------------------------------------------------------------
+ 1. Start Recurrsion from the beginning of the both strings.
+ 2.     If the s index equals to the end of the s string, have found the full match, exit from recursion.
+ 3.     If the t index equals to the end of the t string, have NOT found match, exit from recursion.
+ 4.     If sIndex char is found in t string
+ 5.         Increment sIndex
+ 6.     Increment tIndex+
+ 7.     Call recursion of the next elements.
+ //-----------------------------------------------------------------------------
  * @param {string} s
  * @param {string} t
  * @return {boolean}
---
+++ leetCode-75/day-2/392/392-is-subsequence-(two-pointers).js
@@ -0,0 +1,39 @@
+/**
+ * Runtime: 73  ms
+ * Memory Usage: 41.7 MB
+ * Link: https://leetcode.com/problems/is-subsequence/submissions/823267304/
+ //-----------------------------------------------------------------------------
+ 1. Define s and t pointers
+ 2. While loop both pointers haven't reached the end of the string
+ 3.     If the char under s and t pointers are equal
+ 4.         Increment s pointer to the next char
+ 5.     Increment t pointer to the next char
+ 6. Check if s pointer reached the end of the string, it means that the stirng was subsequence.
+ //-----------------------------------------------------------------------------
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    
+    // 1. Define s and t pointers
+    let sPointer = 0, tPointer = 0;
+
+    // 2. While loop both pointers haven't reached the end of the string
+    while (sPointer < s.length && tPointer < t.length)
+    {
+
+        // 3. If the char under s and t pointers are equal 
+        if(s[sPointer] == t[tPointer]) {
+
+            // 4. Increment s pointer to the next char
+            sPointer++;
+        }
+
+        // 5. Increment t pointer to the next char
+        tPointer++;
+    }
+
+    // 6. Check if s pointer reached the end of the string, it means that the stirng was subsequence.
+    return sPointer == s.length;
+}
