716d7dab4839d7718778cbf2cd80ab9ebcab344b
antonborodulin <aborodulin@planatechnologies.com>
2022-10-14 18:14:22 -0700

    +/- Update 121 task, best-time-to-buy-and-sell-stock

--- data-structure/day-3/121/solution-1-(easer-and-slower)/121-best-time-to-buy-and-sell-stock.js
+++ data-structure/day-3/121/solution-1-(easer-and-slower)/121-best-time-to-buy-and-sell-stock.js
@@ -1,7 +1,15 @@
 /**
- * Runtime: 119 ms
- * Memory Usage: 52 MB
- * Link: https://leetcode.com/submissions/detail/762979232/
+ * Runtime: 79 ms
+ * Memory Usage: 52.4 MB
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/822667170
+ * //-----------------------------------------------------------------------------
+ *    1. Set the min pointer as max
+ *    2. Set the max pointer as min
+ *    3. Loop over all prices
+ *    4.    Try to find a new min
+ *    5.    Try to find a new max profit.
+ *    6. Return max profit.
+ * //-----------------------------------------------------------------------------
  * @param {number[]} prices
  * @return {number}
  */
