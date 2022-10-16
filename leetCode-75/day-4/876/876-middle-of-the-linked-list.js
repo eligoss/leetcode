c40f144f87c2ab2e85d6c81fb09e350ad6f31612
antonborodulin <aborodulin@planatechnologies.com>
2022-10-16 00:11:19 -0700

    +/- 876 middle of the linked list

--- leetCode-75/day-4/876/876-middle-of-the-linked-list.js
+++ leetCode-75/day-4/876/876-middle-of-the-linked-list.js
@@ -1,53 +1,33 @@
 /**
  * Runtime: 59 ms
- * Memory Usage: 42.1 MB
- * Link: https://leetcode.com/submissions/detail/762942716/
- /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+ * Memory Usage: 41.5 MB
+ * Link: https://leetcode.com/problems/middle-of-the-linked-list/submissions/823568464/
+ //-----------------------------------------------------------------------------
+ 1. Define slow and fast pointer
+ 2. While fast pointer is not null and next is not null
+ 3.     Set the slow to the next.
+ 4.     Set the fast to the next.
+ 5. Return slow pointer.
+ //-----------------------------------------------------------------------------
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var middleNode = function(head) {
 
-    // 1. Check that the head is not null
-    if(head == null) {
-        return null;
+    // 1. Define slow and fast pointer
+    var slowPointer = head, fastPointer = head;
+
+    // 2. While fast pointer is not null and next is not null
+    while(fastPointer != null && fastPointer.next != null)
+    {
+        // 3. Set the slow to the next.
+        slowPointer = slowPointer.next;
+
+        // 4. Set the fast to the next.
+        fastPointer = fastPointer.next.next;
     }
 
-    // 2. Check that the next is not null
-    if(head.next == null) {
-        return head;
-    }
-
-    // 3. Create slow and fast iterator
-    // 4. Slow iterator iterates with x1 speed
-    let slow = head;
-
-    // 5. Fast iterator iterates with x2 speed so it starts from the next element
-    let fast = head.next;
-
-    // 6. Loop while the fast don't find the end of the list
-    while(fast != null) {
-
-        // 7. iterate slow to the next item
-        slow = slow.next;
-
-        // 8. If the next of the fast is set, set fast to next x2
-        if(fast.next != null) {
-            fast = fast.next.next;
-        }
-        // 9. Else fast is null, exist the loop
-        else {
-            fast = null;
-        }
-    }
-
-    //10. Return the result
-    return slow;
+    // 5. Return slow pointer.
+    return slowPointer;
 };
