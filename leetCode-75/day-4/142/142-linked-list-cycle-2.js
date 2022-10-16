4f002b07376011fa3fcac509c6e497518da4371b
antonborodulin <aborodulin@planatechnologies.com>
2022-10-16 00:55:12 -0700

    +/- 142 linked list cycle-2 updates

--- leetCode-75/day-4/142/142-linked-list-cycle-2.js
+++ leetCode-75/day-4/142/142-linked-list-cycle-2.js
@@ -1,46 +1,43 @@
 /**
- * Runtime: 110 ms
- * Memory Usage: 45 MB
- * Link: https://leetcode.com/submissions/detail/762966699/
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
+ * Runtime: 89 ms
+ * Memory Usage: 45.6 MB
+ * Link: https://leetcode.com/problems/linked-list-cycle-ii/submissions/823587529/
+ *  //-----------------------------------------------------------------------------
+ *  1. Define has set
+ *  2. Backup head for the iterations.
+ *  3. Interate over the list unit it is not ended
+ *  4.     If the hash set contains the node, return the node as result
+ *  5      Add the node to hash set
+ *  6.     Move current to the next element
+ *  7.  Return null if the is no loop.
+ *  //-----------------------------------------------------------------------------
+ *
  * @param {ListNode} head
  * @return {ListNode}
  */
 var detectCycle = function(head) {
 
-    // 1. If the head is null, return null
-    if(head == null) {
-        return null;
-    }
+    // 1. Define has set
+    let set = new Set();
 
-    // 2. Create hash set
-    var set = new Set();
+    // 2. Backup head for the iterations.
+    var current = head;
 
-    // 3. Define current iterator
-    let current = head;
-
-    // 4. Loop while the current is not null
+    // 3. Interate over the list unit it is not ended
     while(current != null) {
 
-        // 5. If the hash set contains the element, it is the loop, return current;
+        // 4. If the hash set contains the node, return the node as result
         if(set.has(current)){
             return current;
         }
 
-        // 6. Add the current to the set
+        // 5. Add the node to hash set
         set.add(current);
 
-        // 7. Move iterator to the next element
+        // 6. Move current to the next element
         current = current.next;
     }
 
-    //8. Return null as negative scenario
+    // 7. Return null if the is no loop.
     return null;
 };
